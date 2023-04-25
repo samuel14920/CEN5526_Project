65d77e13615c6df430ba78cf16cbac36168a39bb
benjaminfriedman1 <104776859+benjaminfriedman1@users.noreply.github.com>
2023-04-25 19:47:02 -0400

    Add files via upload
    fixes

--- script.py
+++ script.py
@@ -5,17 +5,24 @@ from sklearn.model_selection import train_test_split
 app_data = pd.read_csv('app_data.csv')
 mood_data = pd.read_csv('mood_data.csv')
 
-# Normalize the time in app_data to match the times in mood_data
-app_data['Time'] = pd.to_datetime(app_data['Time'])
-app_data['Time'] = app_data['Time'].dt.floor('H')
-app_data['Time'] = app_data['Time'].dt.strftime('%I:%M%p').str.lower()
+# # Normalize the time in app_data to match the times in mood_data
+# app_data['Time'] = pd.to_datetime(app_data['Time'])
+# app_data['Time'] = app_data['Time'].dt.floor('H')
+# app_data['Time'] = app_data['Time'].dt.strftime('%I:%M%p').str.lower()
 
 # Merge the datasets on date and time
-merged_data = pd.merge(app_data, mood_data, on=['Date', 'Time'], how='inner')
-
+# merged_data = pd.merge(app_data, mood_data, on=['Date', 'Time'], how='inner')
+merged_data = pd.merge(app_data, mood_data, on=['Date'], how='inner')
+print(merged_data)
 # Split the data into training and testing sets
-X = merged_data[['Duration', 'App name']]
+X = merged_data[['Duration_Seconds', 'App name']]
 y = merged_data['Happiness']
+
+# print(X)
+
+# print("test")
+
+# print(y)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Fit a linear regression model
